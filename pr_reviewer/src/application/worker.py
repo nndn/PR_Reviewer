387b2596ea32d9cdaa7f5c57707ee7fa798b8d76
nndn <mynameisgurunandan@gmail.com>
2025-01-22 10:23:44 +0530

    feat: retry task on failure, set as failed if task fails on 3 retries

--- pr_reviewer/src/application/worker.py
+++ pr_reviewer/src/application/worker.py
@@ -4,6 +4,7 @@ from src.ports.pull_request_repo import IPullRequestRepo
 from src.ports.task_repo import ITaskRepo
 from src.ports.reviewer import IPullRequestReviewer
 from src.ports.task_execution import ITaskConsumer
+from retry import retry
 
 
 class WorkerApplication(ITaskConsumer): 
@@ -13,13 +14,21 @@ class WorkerApplication(ITaskConsumer):
         self.pr_repo = pr_repo
         self.task_repo = task_repo
         self.reviewer = reviewer
-        
-    def on_task(self, task_id:int):
+    
+    @retry(tries=3)        
+    def try_task(self, task_id:int):
+        self.logger.info(f"trying task_id: {task_id}")
         self.task_repo.update(task_id, TaskStatus.PROCESSING)
         task = self.task_repo.get(task_id)
         pr = self.pr_repo.get(task.repo_url, task.pr_number, auth_token=task.auth_token)
-        
+            
         # pr review logic
         review = self.reviewer.review_pr(pr)
     
-        self.task_repo.update(task.id, TaskStatus.COMPLETED, review)     
+        self.task_repo.update(task.id, TaskStatus.COMPLETED, review)
+    
+    def on_task(self, task_id:int):
+        try:
+            self.try_task(task_id)
+        except:
+            self.task_repo.update(task_id, TaskStatus.FAILED)
